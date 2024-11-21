e057d8abfaf163159a3b5005f8fb3c6564b4cd1a
Andersoniza <ajc.iza@yavirac.edu.ec>
2024-11-21 17:41:30 -0500

    Clase15Back

--- src/modules/categoria/categoria.service.ts
+++ src/modules/categoria/categoria.service.ts
@@ -3,17 +3,20 @@ import { CreateCategoriaDto } from './dto/create-categoria.dto';
 import { UpdateCategoriaDto } from './dto/update-categoria.dto';
 import { Repository } from 'typeorm';
 import { Categoria } from './entities/categoria.entity';
+import { CreateClienteDto } from '../cliente/dto/create-cliente.dto';
 
 @Injectable()
 export class CategoriaService {
 
   constructor(@Inject('CATEGORIA_REPOSITORY') private categoriaRepository:Repository<Categoria>){}
 
-  
-  create(createCategoriaDto: CreateCategoriaDto) {
-    return 'This action adds a new categoria';
-  }
+  async create(createCategoriaDto: CreateCategoriaDto){
+    const categoria = new Categoria()
+    categoria.nombre=createCategoriaDto.nombre
+    categoria.detalle=createCategoriaDto.detalle
+    return await this.categoriaRepository.save(categoria)
 
+  }
   async findAll() {
     return await this.categoriaRepository.find();
   }
